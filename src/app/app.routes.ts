c360e1342e05a7771a4fca67c372a75cdec9d721
smm_2271 <zalicleexu@gmail.com>
2025-12-28 09:56:09 +0000

    fix: 修正了守衛設置錯誤導致已登入使用者可訪問/signin的問題

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Chat } from './pages/chat-page/chat-page';
 import { authGuard } from './guards/auth.guard';
+import { guestGuard } from './guards/guest.guard';
 
 export const routes: Routes = [
     {
@@ -12,6 +13,7 @@ export const routes: Routes = [
         path: 'signin',
         loadComponent: () =>
             import('./pages/auth-page/auth-page').then(m => m.AuthPage)
+        , canActivate: [guestGuard]
     },
     {
         path: 'chat',
